00ba0673fe4b1a9e2094d2020a9d4e6f3b3cd44f
Wilson Yuri <wilsonyuri@icloud.com>
2024-03-24 12:39:07 -0300

    add new archives

---
+++ 2936.py
@@ -0,0 +1,8 @@
+Curu = int(input()) * 300
+Boit = int(input()) * 1500
+Boto = int(input()) * 600
+Mapi = int(input()) * 1000
+Iara = int(input()) * 150
+Dona = 225
+
+print(f'{Curu+Boit+Boto+Mapi+Iara+Dona}')
